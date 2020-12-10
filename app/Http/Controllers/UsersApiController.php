d2adf4f26df31ae016a7bc18280448e4dbe44c9d
husnainahmed61 <36857205+husnainahmed61@users.noreply.github.com>
2020-12-10 17:41:55 +0100

    user registeration via api

--- app/Http/Controllers/UsersApiController.php
+++ app/Http/Controllers/UsersApiController.php
@@ -12,19 +12,45 @@ class UsersApiController extends Controller
 {
     public function store(RegisterUserRequest $request)
     {
-        $data = $request->all();
+        $user = new User();
+        $user->role_id = 2;
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->contact = $request->contact;
+        $user->postal_address = $request->postal_address;
+        $user->permenant_address = $request->permenant_address;
+        $user->postal_code = $request->postal_code;
+        $user->city = $request->city;
+        $user->country = $request->country;
+        $user->state = $request->state;
+        $user->delievery_type = $request->delievery_type;
+        $user->packaging_type = $request->packaging_type;
+        $user->preferred_delivery_window = $request->preferred_delivery_window;
+        $user->email = $request->email;
+        $user->password =Hash::make($request->password);
 
-        $data['password'] = Hash::make($data['password']);
-        $data['role_id'] = 2;
+        $result = $user->save();
 
-        print_r($data);
+        if ($result){
+            return ["Result" => "Data has been saved"];
+        } else{
+            return ["Result" => "Failed to Save"];
+        }
 
-        $user = User::create($data);
-        var_dump($user);
-        exit();
-        return (new UserResource($user))
-            ->response()
-            ->setStatusCode(Response::HTTP_CREATED);
+//
+//        $data = $request->all();
+//
+//        $data['password'] = Hash::make($data['password']);
+//        $data['role_id'] = 2;
+//
+//        print_r($data);
+//
+//        $user = User::create($data);
+//        var_dump($user);
+//        exit();
+//        return (new UserResource($user))
+//            ->response()
+//            ->setStatusCode(Response::HTTP_CREATED);
 
         //return $this->respondWithToken($token);
 
