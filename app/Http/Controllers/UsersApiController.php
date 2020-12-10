e730368f57211c8cb655a84f6a9537bf1417c34b
husnainahmed61 <36857205+husnainahmed61@users.noreply.github.com>
2020-12-10 17:46:56 +0100

    Update UsersApiController.php

--- app/Http/Controllers/UsersApiController.php
+++ app/Http/Controllers/UsersApiController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterUserRequest;
 use App\Models\User;
-use App\Http\Resources\User as UserResource;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -32,47 +31,13 @@ class UsersApiController extends Controller
         $result = $user->save();
 
         if ($result){
-            return ["Result" => "Data has been saved"];
+            return ["Respone" => Response::HTTP_CREATED,
+                "Result" => "Data has been saved",
+                "Data" => $result];
         } else{
-            return ["Result" => "Failed to Save"];
+            return ["Respone" => Response::HTTP_FORBIDDEN,
+                "Result" => "Failed to Save"];
         }
 
-//
-//        $data = $request->all();
-//
-//        $data['password'] = Hash::make($data['password']);
-//        $data['role_id'] = 2;
-//
-//        print_r($data);
-//
-//        $user = User::create($data);
-//        var_dump($user);
-//        exit();
-//        return (new UserResource($user))
-//            ->response()
-//            ->setStatusCode(Response::HTTP_CREATED);
-
-        //return $this->respondWithToken($token);
-
-        //$data['role_id'] = 2;
-//
-//        return User::create([
-//            'role_id'=>$data['role_id'],
-//            'first_name' => $data['first_name'],
-//            'last_name' => $data['last_name'],
-//            'contact' => $data['contact'],
-//            'postal_address' => $data['postal_address'],
-//            'permenant_address' => $data['permenant_address'],
-//            'postal_code' => $data['postal_code'],
-//            'city' => $data['city'],
-//            'country' => $data['country'],
-//            'state' => $data['state'],
-//            'delievery_type' => $data['delievery_type'],
-//            'packaging_type' => $data['packaging_type'],
-//            'preferred_delivery_window' => $data['preferred_delivery_window'],
-//            'email' => $data['email'],
-//            'password' => Hash::make($data['password']),
-//
-//        ]);
     }
 }
